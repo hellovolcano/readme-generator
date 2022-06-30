742d615e86d33e3203950dc6d344b050796fe480
hellovolcano <valerie.gleason@gmail.com>
2022-06-30 10:19:16 -0500

    Rendering license badges

--- dist/readme.md
+++ dist/readme.md
@@ -1,9 +1,10 @@
-# Portfolio: Valerie Gleason
-  ISC
+# v
+  [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+    
   
   ## Description
 
-  A portfolio of my work
+  v
   
   ## Table of Contents
   * [Installation](#installation)
@@ -11,31 +12,32 @@
   * [Contributing](#contributing)
   * [Tests](#tests)
   * [Questions](#questions)
+  * [License](#license)
 
   
   The TOC will be added here
   
 
   ## Installation
-  N/A
+  v
 
   ## Usage
-  N/A
+  v
 
   ## Contributing
 
-  Don't
+  v
 
   ## Tests
-  N/A
+  v
 
   ## Questions
 
-  To report issues, contact Valerie at valerie.gleason@gmail.com
+  To report issues, contact v at v
 
   ## License
   
   This project is covered by the 
-  [ISC](https://www.google.com)
+  [GPL 3.0](https://www.google.com)
    license.
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,15 +4,20 @@ const fs = require('fs')
 // If there is no license, return an empty string
 const renderLicenseBadge = license => {
   if (license == 'MIT') {
-    return ` MIT`
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    `
   } else if (license === 'ISC') {
-    return `ISC`
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+      `
   } else if (license === 'Apache 2.0') {
-    return `Apache 2.0`
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+    `
+  } else if (license === 'BSD 3-Clause') {
+    return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+    `
   } else if (license === 'GPL 3.0') {
-    return `GPL 3.0`
-  } else if (license === 'ISC') {
-    return `ISC`
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+    `
   } else {
     return ''
   }
